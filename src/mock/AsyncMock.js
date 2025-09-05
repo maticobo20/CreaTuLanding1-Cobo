f72ea9a86f37bb2bb86ec22ee4b6a2661c975415
Matias Cobo <maticobo20@gmail.com>
2025-09-05 18:44:26 -0300

    firebase

--- src/mock/AsyncMock.js
+++ src/mock/AsyncMock.js
@@ -1,4 +1,3 @@
-// Permite actualizar el stock de un producto por id
 export const updateStock = (id, quantity) => {
   const prod = productos.find(p => p.id === id);
   if (prod && prod.stock >= quantity) {
@@ -11,92 +10,91 @@ const productos = [
     {
         id: '01',
         name: 'Remera Human Made',
-        price: 20000,
         description: 'Remera oversize\n100% algodón\nColores: Blanco y Negro',
         category: 'remeras',
         stock: 5,
+        price: 20000,
         img: '/human.png',
         
     },
     {
         id: '02',
         name: 'Remera 11:11',
-        price: 20000,
         description: 'Remera oversize\n100% algodón\nColores: Blanco y Negro',
         category: 'remeras',
         stock: 5,
+        price: 20000,
         img: '/11.png',
         
     },
     {
         id: '03',
         name: 'Remera Born Wild',
-        price: 20000,
         description: 'Remera oversize\n100% algodón\nColores: Blanco y Negro',
         category: 'remeras',
         stock: 5,
+        price: 20000,
         img: '/pantera.png',
         
     },
     {
         id: '04',
         name: 'Buzo Crewneck',
-        price: 75000,
         description: 'Prenda estampada\nAlgodón rustico\nCalidad premium\nCorte oversize',
         category: 'buzos',
         stock: 5,
+        price: 75000,
         img: '/buzo4vx.png',
         
     },
     {
         id: '05',
         name: 'Hoodie Joker\'s Delight',
-        price: 75000,
         description: 'Prenda estampada\n100% algodón\nCalidad premium\nCorte oversize',
         category: 'buzos',
         stock: 5,
+        price: 75000,
         img: '/buzo-calavera.png',
         
     },
     {
         id: '06',
         name: 'Hoodie The Present is Forever',
-        price: 75000,
         description: '100% algodón\nPrenda estampada\nCalidad premium\nCorte oversize',
         category: 'buzos',
         stock: 5,
+        price: 75000,
         img: '/buzo-hoodie.png',
         
     },
     {
         id: '07',
         name: 'Hoodie Department',
-        price: 75000,
         description: '100% algodón\nPrenda estampada\nCalidad premium\nCorte oversize',
         category: 'buzos',
         stock: 5,
+        price: 75000,
         img: '/buzo-depart.png',
         
     },
     {
         id: '08',
         name: 'Jogger - Gang',
-        price: 70000,
         description: 'Algodón rustico\nPrenda estampada\nCalidad premium\nCorte oversize',
         category: 'pantalones',
         stock: 5,
+        price: 70000,
         img: '/jogger.jpg',
         
     },
     {
         id: '09',
         name: 'Campera Puffer 4VX',
-        price: 250000,
         description: 'Prenda estampada\nCalidad IMPORTADA\nCorte oversize',
         category: 'camperas',
         stock: 5,
+        price: 250000,
         img: '/campera.png',
-        
     }
 ]
 
---
+++ src/service/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOcvEC_WuXCG9hZg5Ni5HRnsIX8Qa9rzA",
+  authDomain: "melek-f3b93.firebaseapp.com",
+  projectId: "melek-f3b93",
+  storageBucket: "melek-f3b93.firebasestorage.app",
+  messagingSenderId: "877063035837",
+  appId: "1:877063035837:web:700be3a2a2974ef1bbd0d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
